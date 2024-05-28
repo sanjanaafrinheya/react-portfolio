d9d142d0492b201ff3a846036aa3dda6326e95e8
sanjanaafrinheya <sanjanaafrinheya@gmail.com>
2024-05-29 00:59:36 +0600

    start

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -24,7 +24,7 @@ const Service = () => {
            <Servicereusable  icon={<FaCamera />} title="Photography"para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eveniet tenetur vitae!"/>  
            <Servicereusable  icon={<HiMiniMegaphone />} title="Digital Marketing"para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eveniet tenetur vitae!"/>  
            <Servicereusable  icon={<FaFileVideo />} title="Video Editing"para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eveniet tenetur vitae!"/>  
-           <Servicereusable  icon={<FaMagnifyingGlassDollar />} title="SEO MArketing"para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eveniet tenetur vitae!"/>  
+           <Servicereusable  icon={<FaMagnifyingGlassDollar />} title="SEO Marketing"para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eveniet tenetur vitae!"/>  
         </Row>
     </Container>
    </section>
