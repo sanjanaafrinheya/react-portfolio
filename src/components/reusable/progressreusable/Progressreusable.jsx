588ac59327c4e8cb23fcb11f42c6b941040cbe7b
sanjanaafrinheya <sanjanaafrinheya@gmail.com>
2024-05-30 20:26:25 +0600

    mine

--- src/components/reusable/progressreusable/Progressreusable.jsx
+++ src/components/reusable/progressreusable/Progressreusable.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-// import ProgressBar from "@ramonak/react-progress-bar";
-import ProgressBar from 'react-bootstrap/ProgressBar';
-const Progressreusable = () => {
-  return;
-  <ProgressBar now={now} label={`${70}%`}  />;
+import ProgressBar from "@ramonak/react-progress-bar";
+import "./Progressreusable.css"
+const Progressreusable = (props) => {
+  return (
+    <div className="black">
+      <div className="star d-flex ">
+        <div className="one">
+        <h4>{props.name}</h4>
+        </div>
+        <div className="two">
+          <span>{props.parcent}</span>
+        </div>
+      </div>
+<ProgressBar completed={props.num}
+             animateOnRender={true} 
+             transitionDuration="6s"
+              isLabelVisible={false}
+              bgColor="#bc8cf2"
+              baseBgColor="#222"
+              height="10px"
+              />
+              
+    </div>
+  )
 }
-
 
 export default Progressreusable
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -9,12 +9,27 @@ const Skills = () => {
   return (
    <section id="skill">
     <Container>
-{/* <div data-aos="fade-up-left">
+<div data-aos="fade-up">
       <Headerresuable main="My" para="Skills"/>
-      </div> */}
+      </div>
         <Row>
               <Col lg={6}>
-              <Progressreusable/>
+              <Progressreusable num="95" name="HTML 5" parcent="95%"/>
+              </Col>
+              <Col lg={6}>
+              <Progressreusable num="75" name="JQuery" parcent="75%" />
+              </Col>
+              <Col lg={6}>
+              <Progressreusable num="90" name="CSS3" parcent="90%" />
+              </Col>
+              <Col lg={6}>
+              <Progressreusable num="65" name="Bootstrap" parcent="65%" />
+              </Col>
+              <Col lg={6}>
+              <Progressreusable num="85" name="Javascript" parcent="85%" />
+              </Col>
+              <Col lg={6}>
+              <Progressreusable num="60" name="React" parcent="60%" />
               </Col>
             </Row>
      
